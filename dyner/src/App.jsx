c030af719a2de0248eeb09aaf10953b2293027a4
yerin <yerin6428@gmail.com>
2025-07-07 21:02:55 +0900

    수정

--- dyner/src/App.jsx
+++ dyner/src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import Developers from './Developers';
+import SignIn from './SignIn';
+import SignUp from './SignUp';
+import Projects from './Projects';
 
 export default function App() {
   return (
@@ -8,6 +11,9 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/developers" element={<Developers />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/projects" element={<Projects />} />
       </Routes>
     </BrowserRouter>
   );
